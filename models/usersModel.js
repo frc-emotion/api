275d0726778f4757a5ea254e5dd9d7e04c55fb6d
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-12-12 02:01:18 -0800

    fix user model

--- models/usersModel.js
+++ models/usersModel.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const usersSchema = mongoose.Schema(
     {
-        firstname: {type: String},
-        lastname: {type: String},
-        username: {type: String},
-        email: {type: String},
-        password: {type: String},
-        isAdmin: {type: String},
-        isVerified: {type: String},
+        firstname: { type: String, required: true },
+        lastname: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        isAdmin: { type: Boolean, default: false },
+        isVerified: { type: Boolean, default: false },
     },
     {
         timestamps: true,
