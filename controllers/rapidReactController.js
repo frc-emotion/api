b99f05cf4930956890321a5419f8d6db8e27d851
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-12-24 17:07:46 -0800

    update get request paramaters

--- controllers/rapidReactController.js
+++ controllers/rapidReactController.js
@@ -1,106 +1,117 @@
 const asyncHandler = require("express-async-handler");
 const Game = require("../models/rapidReactModel.js");
 const fetch = (...args) =>
-	import("node-fetch").then(({ default: fetch }) => fetch(...args));
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 async function create(req, nickname) {
-	return await Game.create({
-		competition: req.body.competition,
-		matchNumber: req.body.matchNumber,
-		teamNumber: req.body.teamNumber,
-		teamName: nickname,
-		tarmac: req.body.tarmac,
-		autoLower: req.body.autoLower,
-		autoUpper: req.body.autoUpper,
-		teleopLower: req.body.teleopLower,
-		teleopUpper: req.body.teleopUpper,
-		cycleTime: req.body.cycleTime,
-		mainShots: req.body.mainShots,
-		climbScore: req.body.climbScore,
-		defensive: req.body.defensive,
-		humanShot: req.body.humanShot,
-		rankingPoints: req.body.rankingPoints,
-		score: req.body.score,
-		won: req.body.won,
-		comments: req.body.comments,
-	});
+    return await Game.create({
+        competition: req.body.competition,
+        matchNumber: req.body.matchNumber,
+        teamNumber: req.body.teamNumber,
+        teamName: nickname,
+        tarmac: req.body.tarmac,
+        autoLower: req.body.autoLower,
+        autoUpper: req.body.autoUpper,
+        teleopLower: req.body.teleopLower,
+        teleopUpper: req.body.teleopUpper,
+        cycleTime: req.body.cycleTime,
+        mainShots: req.body.mainShots,
+        climbScore: req.body.climbScore,
+        defensive: req.body.defensive,
+        humanShot: req.body.humanShot,
+        rankingPoints: req.body.rankingPoints,
+        score: req.body.score,
+        won: req.body.won,
+        comments: req.body.comments,
+    });
 }
 
 // @desc Get Games
-// @route GET /api/games/{games[i].name}/
+// @route GET /api/rapidReact/
+// @route GET /api/rapidReact/:id
+// @route GET /api/rapidReact?competition={competition}
 // @access Private
 const getGames = asyncHandler(async (req, res) => {
-	const games = await Game.find({ });
-	res.status(200).json(games);
+    // 3 cases: id provided, competition queried, or all
+    const games = req.params.id
+        ? // find game by id if id provided
+          await Game.findById(req.params.id)
+        : req.query.competition
+        ? // find game by competition if queried
+          await Game.find({ competition: req.query.competition })
+        : // otherwise show all games for the season
+          await Game.find({});
+
+    res.status(200).json(games);
 });
 
 // @desc Set Game
-// @route POST /api/games/{games[i].name}/
+// @route POST /api/rapidReact/
 // @access Private
 const setGame = asyncHandler(async (req, res) => {
-	const key = process.env.TBA_KEY;
+    const key = process.env.TBA_KEY;
 
-	const request = await fetch(
-		"https://www.thebluealliance.com/api/v3/team/frc" + req.body.teamNumber,
-		{ method: "GET", headers: { "X-TBA-Auth-Key": key } }
-	);
-	const jsoned = await request.json();
-	if (request.status != 200) {
-		try {
-			await create(req, "-1").then((game) => {
-				res.status(200).json(game);
-			});
-		} catch (ValidationError) {
-			res.status(400).json({ error: "Invalid request body" });
-		}
-	} else {
-		try {
-			await create(req, jsoned["nickname"]).then((game) => {
-				res.status(200).json(game);
-			});
-		} catch (ValidationError) {
-			res.status(400).json({ error: "Invalid request body" });
-		}
-	}
+    const request = await fetch(
+        "https://www.thebluealliance.com/api/v3/team/frc" + req.body.teamNumber,
+        { method: "GET", headers: { "X-TBA-Auth-Key": key } }
+    );
+    const jsoned = await request.json();
+    if (request.status != 200) {
+        try {
+            await create(req, "-1").then((game) => {
+                res.status(200).json(game);
+            });
+        } catch (ValidationError) {
+            res.status(400).json({ error: "Invalid request body" });
+        }
+    } else {
+        try {
+            await create(req, jsoned["nickname"]).then((game) => {
+                res.status(200).json(game);
+            });
+        } catch (ValidationError) {
+            res.status(400).json({ error: "Invalid request body" });
+        }
+    }
 });
 
 // @desc Update Game
-// @route PUT /api/games/{games[i].name}/:id
+// @route PUT /api/rapidReact/:id
 // @access Private
 const updateGame = asyncHandler(async (req, res) => {
-	const game = await Game.findById(req.params.id);
+    const game = await Game.findById(req.params.id);
 
-	if (!game) {
-		res.status(400);
-		throw new Error("Game not found");
-	}
+    if (!game) {
+        res.status(400);
+        throw new Error("Game not found");
+    }
 
-	const updatedGame = await Game.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-	});
+    const updatedGame = await Game.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
 
-	res.status(200).json(updatedGame);
+    res.status(200).json(updatedGame);
 });
 
 // @desc Delete Game
-// @route DELETE /api/games/{games[i].name}/:id
+// @route DELETE /api/rapidReact/:id
 // @access Private
 const deleteGame = asyncHandler(async (req, res) => {
-	const game = await Game.findById(req.params.id);
+    const game = await Game.findById(req.params.id);
 
-	if (!game) {
-		res.status(400);
-		throw new Error("Game not found");
-	}
+    if (!game) {
+        res.status(400);
+        throw new Error("Game not found");
+    }
 
-	await game.remove();
+    await game.remove();
 
-	res.status(200).json({ id: req.params.id });
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
-	getGames,
-	setGame,
-	updateGame,
-	deleteGame,
+    getGames,
+    setGame,
+    updateGame,
+    deleteGame,
 };
--- routes/rapidReactRoutes.js
+++ routes/rapidReactRoutes.js
@@ -12,6 +12,10 @@ const { verifiedProtect } = require("../middleware/authMiddleware");
 // all routes require user to be verified before making a request
 // authorization middleware checks for bearer token in header
 router.route("/").post(verifiedProtect, setGame).get(verifiedProtect, getGames);
-router.route("/:id").put(verifiedProtect, updateGame).delete(verifiedProtect, deleteGame);
+router
+    .route("/:id")
+    .get(verifiedProtect, getGames)
+    .put(verifiedProtect, updateGame)
+    .delete(verifiedProtect, deleteGame);
 
 module.exports = router;
