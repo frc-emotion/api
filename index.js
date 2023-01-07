f89eefbad9ec3103b4d65fc9f506ac1eab90c63d
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-01-07 13:31:20 -0800

    update api entry

--- index.js
+++ index.js
@@ -6,47 +6,56 @@ const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const api = process.env.API_URL;
 const app = express();
-const games = [{ name: "rapidReact" }];
+const games = [{ name: "rapidReact" }, { name: "chargedUp" }];
 
 // connect to the MongoDB database, configure in ./config/db.js
 
-const gamesDb = connectDB(process.env.MONGO_URI + "games?retryWrites=true&w=majority")
-gamesDb.on('connected', () => {
-    console.log("Successfully connected to games database: " + `${gamesDb.host}`.green.underline)
-  });
-const usersDb = connectDB(process.env.MONGO_URI + "users?retryWrites=true&w=majority");
-usersDb.on('connected', () => {
-    console.log("Successfully connected to users database: " + `${usersDb.host}`.green.underline)
+const gamesDb = connectDB(
+	process.env.MONGO_URI + "games?retryWrites=true&w=majority"
+);
+gamesDb.on("connected", () => {
+	console.log(
+		"Successfully connected to games database: " +
+			`${gamesDb.host}`.green.underline
+	);
+});
+const usersDb = connectDB(
+	process.env.MONGO_URI + "users?retryWrites=true&w=majority"
+);
+usersDb.on("connected", () => {
+	console.log(
+		"Successfully connected to users database: " +
+			`${usersDb.host}`.green.underline
+	);
 });
 
 global.gamesDb = gamesDb;
 global.usersDb = usersDb;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
 });
 
 for (let i = 0; i < games.length; i++) {
-    app.use(
-        `${api}/${games[i].name}`,
-        require(`./routes/${games[i].name}Routes`)
-    );
+	app.use(
+		`${api}/${games[i].name}`,
+		require(`./routes/${games[i].name}Routes`)
+	);
 }
 
-app.use(`${api}/users`, require('./routes/userRoutes.js'))
+app.use(`${api}/users`, require("./routes/userRoutes.js"));
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log("Server started at: " + `localhost:${port}`.underline.green);
+	console.log("Server started at: " + `localhost:${port}`.underline.green);
 });
