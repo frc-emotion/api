64fe58411a298af2445b993bb4e28d38229f53dc
ArshansGithub <111618520+ArshansGithub@users.noreply.github.com>
2023-03-15 18:53:36 -0700

    we dont talk abt it

--- routes/inPitRoutes.js
+++ routes/inPitRoutes.js
@@ -10,7 +10,7 @@ const { verifiedProtect } = require("../middleware/authMiddleware");
 
 // all routes require user to be verified before making a request
 // authorization middleware checks for bearer token in header
-router.route("/getProfile").get(getProfile);
+router.route("/getProfile").get(verifiedProtect, getProfile);
 router.route("/editProfile").post(verifiedProtect, editProfile).delete(verifiedProtect, deleteProfile);
 
 module.exports = router;
