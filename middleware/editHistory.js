6b7ee909cccb18c71d9251a2101f5a3d2f32dbf5
Nova Mondal <rialmondal@gmail.com>
2023-10-08 16:41:08 -0700

    added optional chain to edit history middleware to prevent crashes

--- middleware/editHistory.js
+++ middleware/editHistory.js
@@ -1,10 +1,10 @@
 const editHistory = async (req, res, next) => {
 	const changes = {
-		id: req.user.id,
+		id: req.user?.id,
 		time: new Date().toISOString(),
 	};
 	req.edit = [changes];
-    console.log(req);
+	console.log(req);
 	next();
 };
 
