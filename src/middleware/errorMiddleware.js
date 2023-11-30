c10b3bb740afd236dda0068dba74680e4ec0dacf
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-11-29 17:50:40 -0800

    fix error setting headers in error middleware?

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -2,9 +2,7 @@ const errorHandler = (err, req, res, next) => {
     // if a status code is set when an error is thrown it will be used
     // otherwise an error code of 500 will be thrown
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.status(statusCode);
-
-    res.json({
+    res.status(statusCode).json({
         message: err.message,
         // stack trace will be hidden in production
         stack: process.env.NODE_ENV === "production" ? null : err.stack,
