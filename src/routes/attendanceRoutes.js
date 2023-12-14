fae9141d2bdaea2bb66ab803d557fb3502e48653
Nova Mondal <rialmondal@gmail.com>
2023-12-13 17:53:57 -0800

    admin perms to delete meeting

--- src/routes/attendanceRoutes.js
+++ src/routes/attendanceRoutes.js
@@ -12,7 +12,7 @@ const protect = require("../middleware/authMiddleware");
 router.post("/createMeeting", protect(2, createMeeting));
 router.post("/attendMeeting", protect(0, attendMeeting));
 router.get("/getMeetings", protect(2, getMeetings));
-router.delete("/deleteMeeting/:id", protect(2, deleteMeeting));
+router.delete("/deleteMeeting/:id", protect(3, deleteMeeting));
 router.get("/getAll", protect(3, getAllMeetings));
 
 module.exports = router;
