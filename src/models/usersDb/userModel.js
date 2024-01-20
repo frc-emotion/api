0b4730411be7a4588c1cf41a79063b35409a9297
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-01-20 10:34:21 -0800

    add custom role message to user

--- src/models/usersDb/userModel.js
+++ src/models/usersDb/userModel.js
@@ -1,45 +1,46 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-	{
-		firstname: { type: String, required: true, trim: true },
-		lastname: { type: String, required: true, trim: true },
-		username: { type: String, required: true, unique: true, trim: true },
-		email: { type: String, required: true, unique: true, trim: true },
-		password: { type: String, required: true },
-		phone: { type: String, required: false, unique: true },
-		subteam: { type: String, required: false }, //"software" | "build" | "executive" | "marketing" | "electrical" | "design"
-		grade: { type: Number, required: false },
-		roles: { type: Array, required: true },
-		accountType: { type: Number, required: true, default: 0 },
-		accountUpdateVersion: { type: Number, required: true },
-		forgotPassword: { type: Object, required: false },
-		// forgotPassword: {
-		// code: { type: String },
-		// expiresAt: { type: Number },
-		// },
-		rateLimit: { type: Object, required: false },
-		// rateLimit: {
-		// count: Number,
-		// expiresAt: Number,
-		// },
-		socials: { type: Array, required: false },
+    {
+        firstname: { type: String, required: true, trim: true },
+        lastname: { type: String, required: true, trim: true },
+        username: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, trim: true },
+        password: { type: String, required: true },
+        phone: { type: String, required: false, unique: true },
+        subteam: { type: String, required: false }, //"software" | "build" | "executive" | "marketing" | "electrical" | "design"
+        grade: { type: Number, required: false },
+        roles: { type: Array, required: true },
+        customRoleMessage: { type: String, required: false },
+        accountType: { type: Number, required: true, default: 0 },
+        accountUpdateVersion: { type: Number, required: true },
+        forgotPassword: { type: Object, required: false },
+        // forgotPassword: {
+        // code: { type: String },
+        // expiresAt: { type: Number },
+        // },
+        rateLimit: { type: Object, required: false },
+        // rateLimit: {
+        // count: Number,
+        // expiresAt: Number,
+        // },
+        socials: { type: Array, required: false },
 
-		parents: { type: Array, required: false },
-		attendance: { type: Array, required: false }, //array of attendance objects, sorted by semester with 0 = 2023 fall
+        parents: { type: Array, required: false },
+        attendance: { type: Array, required: false }, //array of attendance objects, sorted by semester with 0 = 2023 fall
 
-		children: { type: Array, required: false },
-		spouse: { type: Object, required: false },
-		donationAmounts: { type: Array, required: false },
-		employer: { type: Object, required: false },
+        children: { type: Array, required: false },
+        spouse: { type: Object, required: false },
+        donationAmounts: { type: Array, required: false },
+        employer: { type: Object, required: false },
 
-		// temporary while transitioning from v1 to v2
-		isVerified: { type: Boolean, required: false },
-		isAdmin: { type: Boolean, required: false },
-	},
-	{
-		timestamps: true,
-	}
+        // temporary while transitioning from v1 to v2
+        isVerified: { type: Boolean, required: false },
+        isAdmin: { type: Boolean, required: false },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = global.usersDb.model("User", userSchema);
